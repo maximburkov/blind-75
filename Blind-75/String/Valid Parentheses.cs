d41940c056198df2bd9fb5c0d9c68c0414b594fa
maximburkov <maxburkov12@gmail.com>
2022-06-26 20:16:53 +0400

    Valid Parentheses solved.

---
+++ Blind-75/String/Valid Parentheses.cs
@@ -0,0 +1,48 @@
+﻿namespace Blind_75.String;
+
+public class Valid_Parentheses : ISolution
+{
+    public void Solve()
+    {
+        var res = IsValid("");
+    }
+    
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new();
+
+        if (s.Length % 2 != 0) return false;
+
+        foreach (var character in s)
+        {
+            if (IsOpening(character))
+            {
+                stack.Push(character);
+            }
+            else
+            {
+                if (stack.TryPop(out var popped))
+                {
+                    if (!IsMatching(popped, character))
+                        return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    private bool IsOpening(char character) => character is '(' or '[' or '{';
+
+    private bool IsMatching(char opening, char closing) => opening switch
+    {
+        '(' => closing == ')',
+        '[' => closing == ']',
+        '{' => closing == '}',
+        _ => throw new ArgumentException()
+    };
+}
