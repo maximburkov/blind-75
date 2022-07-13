067ab30750b0bb7a92774484b65cb45c6a8aed80
maximburkov <maxburkov12@gmail.com>
2022-07-13 23:53:34 +0400

    - Cycle in Linked list - Balanced Binary tree

---
+++ dotnet/Blind-75/LinkedList/LinkedListCycle.cs
@@ -0,0 +1,54 @@
+﻿namespace Blind_75.LinkedList;
+
+#nullable disable
+public class LinkedListCycle : ISolution
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public void Solve()
+    {
+        
+    }
+
+    public bool HasCycle(ListNode head)
+    {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        if (fast is null)
+            return false;
+        else
+        {
+            fast = fast.next;
+            if (fast is null)
+                return false;
+        }
+
+        while (fast != slow)
+        {
+            slow = slow.next;
+            fast = fast.next;
+
+            if (fast is null)
+                return false;
+            else
+            {
+                fast = fast.next;
+                if (fast is null)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ dotnet/Blind-75/Tree/BalancedBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿namespace Blind_75.Tree;
+
+#nullable disable
+public class BalancedBinaryTree : ISolution
+{
+    public void Solve()
+    {
+        
+    }
+    
+    public bool IsBalanced(TreeNode root)
+    {
+        if (root is null)
+            return true;
+        
+        int rh = FindHeight(root.right, 0);
+        int lh = FindHeight(root.left, 0);
+
+        return Math.Abs(lh - rh) <= 1 && IsBalanced(root.left) && IsBalanced(root.right);
+    }
+
+    public int FindHeight(TreeNode node, int height)
+    {
+        if (node is null)
+            return height;
+
+        height++;
+
+        return Math.Max(FindHeight(node.left, height), FindHeight(node.right, height));
+    }
+}
