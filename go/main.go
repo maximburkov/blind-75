3af4edf4d827b3087ce77e6a04db7120b95e7a78
maximburkov <maxburkov12@gmail.com>
2022-10-27 23:55:07 +0400

    Golang code reorganized

--- go/main.go
+++ go/main.go
@@ -5,25 +5,6 @@ import (
 )
 
 func main() {
-	res := maxProfit([]int{7, 1, 5, 3, 6, 4})
+	res := MaxProfit([]int{7, 1, 5, 3, 6, 4})
 	fmt.Println(res)
-}
-
-func maxProfit(prices []int) int {
-	min := prices[0]
-	max := 0
-
-	for i := 1; i < len(prices); i++ {
-		dif := prices[i] - min
-
-		if dif > max {
-			max = dif
-		}
-
-		if prices[i] < min {
-			min = prices[i]
-		}
-	}
-
-	return max
 }
